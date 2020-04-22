5b20ef0ae123e7b9556c60ef511c10ee85685047
Rhuan <rhuannixon@gmail.com>
2020-04-21 23:44:35 -0300

    set email as unique

--- src/main/java/com/springboot/api/models/User.java
+++ src/main/java/com/springboot/api/models/User.java
@@ -2,6 +2,7 @@ package com.springboot.api.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,13 +14,14 @@ import javax.persistence.Table;
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private String name;
+	@Column(name = "email", unique = true)
 	private String email;
 	private String password;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	public long getId() {
 		return id;
 	}

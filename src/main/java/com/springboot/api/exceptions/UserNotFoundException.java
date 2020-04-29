70a9412373c260a1594b2eddaf455d00a60bea96
Rhuan <rhuannixon@gmail.com>
2020-04-28 23:11:09 -0300

    create exception for user not found

---
+++ src/main/java/com/springboot/api/exceptions/UserNotFoundException.java
@@ -0,0 +1,18 @@
+package com.springboot.api.exceptions;
+
+public class UserNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException() {
+	}
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+	public UserNotFoundException(Exception e) {
+		super(e);
+	}
+
+}
